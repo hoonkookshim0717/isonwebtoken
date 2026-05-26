1dfd81a3482a615a872bbf419aea3a1c6a9584fe
Shim <loveseeker@empas.com>
2026-05-26 22:16:34 +0900

    Cleaned up the directory

--- src/build/Release/.deps/Release/obj.target/addon.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/addon.node := g++ -o Release/obj.target/addon.node -shared -pthread -rdynamic -m64  -Wl,-soname=addon.node -Wl,--start-group Release/obj.target/addon/addon.o Release/obj.target/addon/men.o Release/obj.target/../node_modules/node-addon-api/nothing.a -Wl,--end-group 
---
+++ tests/testAs.js
@@ -0,0 +1,13 @@
+import MEN from '../lib/men.js';
+
+let number1 = -1;
+
+let result = MEN.encodeNumber(number1);
+
+console.log(result);
+
+let string1 = '*B';
+
+result = MEN.decodeNumber(string1);
+
+console.log(result);
